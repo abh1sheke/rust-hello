b4c5684bf1b1e6dc8f362ab66a27f7f5d2d890d2
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-07-06 20:52:33 +0530

    move connection handler to its own module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
+use connection::handle;
 use env_local::{EnvErr, VariableType};
-use std::{
-    fs,
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-    thread,
-    time::Duration,
-};
+use std::net::TcpListener;
 
+pub mod connection;
 pub mod env_local;
 pub mod thread_pool;
 
@@ -26,7 +22,6 @@ fn main() {
             &0
         }
     };
-
     let pool = thread_pool::ThreadPool::new(*pool_size).unwrap_or_else(|err| {
         panic!("Error occured while creating thread pool: {}", err);
     });
@@ -43,28 +38,8 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         pool.execute(|| {
-            handle_connection(stream);
+            handle::handle_connection(stream);
         });
     }
     println!("Shutting down.");
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    println!("{:#?}", request_line);
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "index.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
 }
